ba3638382a07ad596182979c2553511886c590b0
Manuel Mena <manuelmena1993@gmail.com>
2022-10-06 13:39:56 -0300

    fix: show real name in /list endpoint

---
+++ src/logic/slack.ts
@@ -0,0 +1,19 @@
+import { App } from "@slack/bolt"
+import { StringIndexed } from "@slack/bolt/dist/types/helpers"
+
+export async function getUsername(app: App<StringIndexed>, userToken: string, userId: string | null | undefined) {
+  let username = 'Not assigned'
+
+  if (userId) {
+    const response = await app.client.users.profile.get({
+      user: userId,
+      token: userToken
+    })
+
+    console.log(response)
+
+    if (response.profile?.real_name)
+      username = response.profile?.real_name as string
+  }
+  return username
+}
