12632da04e2615247f6fef6923d7b1167f2e0a99
Manuel Mena <manuelmena1993@gmail.com>
2022-10-10 19:02:17 -0300

    feat: log url data

--- src/logic/validation.ts
+++ src/logic/validation.ts
@@ -6,17 +6,40 @@ export function validateAPIKey(globalContext: GlobalContext): IHttpServerCompone
   return async function (context: IHttpServerComponent.DefaultContext, next: () => Promise<IHttpServerComponent.IResponse>): Promise<IHttpServerComponent.IResponse> {
 
     const apiKeyFromEnv = await globalContext.components.config.getString('API_KEY') ?? ''
+    const originFromEnv = await globalContext.components.config.getString('URL_ORIGIN') ?? ''
     const apiKeyFromHeader = context.request.headers.get('crashbot')
+    // const logger = globalContext.components.logs.getLogger('Validate API Key')
     console.log('URL data:')
     console.log(context.url)
 
-    if (apiKeyFromHeader && apiKeyFromHeader == apiKeyFromEnv)
-      return next()
-    else
-      return {
-        status: 403,
-        statusText: "Not allowed. Invalid API key.",
-        body: 'Not allowed. Invalid API key.\n'
-      }
+    if (!apiKeyFromHeader) {
+      console.log('403 Missing API Key')
+      // return {
+      //   status: 403,
+      //   statusText: 'Not allowed. Missing API key.',
+      //   body: 'Not allowed. Missing API key.\n'
+      // }
+    }
+
+    if (apiKeyFromHeader != apiKeyFromEnv) {
+      console.log('403 Invalid API Key')
+      // return {
+      //   status: 403,
+      //   statusText: 'Not allowed. Invalid API key.',
+      //   body: 'Not allowed. Invalid API key.\n'
+      // }
+    }
+
+    if (context.url.origin != originFromEnv) {
+      console.log('403 Invalid origin')
+      // return {
+      //   status: 403,
+      //   statusText: 'Origin is not allowed.',
+      //   body: 'Origin is not allowed.\n'
+      // }
+    }
+
+    
+    return next()    
   }
 }
