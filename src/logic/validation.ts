96a436607eaeb7f43ca243714b96eee470420a14
Manuel Mena <manuelmena1993@gmail.com>
2022-10-11 14:35:20 -0300

    feat: add api key and origin validation (#18)

--- src/logic/validation.ts
+++ src/logic/validation.ts
@@ -2,46 +2,36 @@ import { RoutedContext } from "@well-known-components/http-server"
 import { IHttpServerComponent } from "@well-known-components/interfaces"
 import { GlobalContext } from "../types"
 
-export function validateAPIKey(globalContext: GlobalContext): IHttpServerComponent.IRequestHandler<RoutedContext<GlobalContext, "/list">> {
+export function validate(globalContext: GlobalContext): IHttpServerComponent.IRequestHandler<RoutedContext<GlobalContext, "/list">> {
+  const logger = globalContext.components.logs.getLogger("request validation")
+  
+  function notAllowedResponse(errorMessage: string) {
+    logger.error(errorMessage)
+    return {
+      status: 403,
+      statusText: errorMessage,
+      body: {
+        message: errorMessage
+      }
+    }
+  }
+  
   return async function (context: IHttpServerComponent.DefaultContext, next: () => Promise<IHttpServerComponent.IResponse>): Promise<IHttpServerComponent.IResponse> {
 
     const apiKeyFromEnv = await globalContext.components.config.getString('API_KEY') ?? ''
-    const originFromEnv = await globalContext.components.config.getString('URL_ORIGIN') ?? ''
+    const allowedOrigin = await globalContext.components.config.getString('ALLOWED_ORIGIN') ?? ''
     const apiKeyFromHeader = context.request.headers.get('crashbot')
     const originFromHeader = context.request.headers.get('origin')
-    // const logger = globalContext.components.logs.getLogger('Validate API Key')
-    console.log(context.request.headers)
-    console.log(`apikey from header: ${apiKeyFromHeader}`)
-    console.log(`origin from header: ${originFromHeader}`)
-
-    if (!apiKeyFromHeader) {
-      console.log('403 Missing API Key')
-      // return {
-      //   status: 403,
-      //   statusText: 'Not allowed. Missing API key.',
-      //   body: 'Not allowed. Missing API key.\n'
-      // }
-    }
-
-    if (apiKeyFromHeader != apiKeyFromEnv) {
-      console.log(`403 Invalid API Key: prov ${apiKeyFromHeader} env ${apiKeyFromEnv}`)
-      // return {
-      //   status: 403,
-      //   statusText: 'Not allowed. Invalid API key.',
-      //   body: 'Not allowed. Invalid API key.\n'
-      // }
-    }
-
-    if (originFromHeader != originFromEnv) {
-      console.log(`403 Invalid origin prov ${originFromHeader} env ${originFromEnv}`)
-      // return {
-      //   status: 403,
-      //   statusText: 'Origin is not allowed.',
-      //   body: 'Origin is not allowed.\n'
-      // }
-    }
-
     
+    if (!apiKeyFromHeader)
+      return notAllowedResponse('Not allowed. Missing API key.')
+
+    if (apiKeyFromHeader != apiKeyFromEnv)
+      return notAllowedResponse(`Not allowed. Invalid API key: ${apiKeyFromHeader}.`)
+
+    if (originFromHeader != allowedOrigin)
+      return notAllowedResponse(`Origin not allowed: ${originFromHeader}.`)
+
     return next()    
   }
 }
