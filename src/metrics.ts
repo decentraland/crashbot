01863bca9d1b5a6d3220202018666ba861ada633
Hugo Arregui <hugo@decentraland.org>
2023-11-30 18:10:38 -0300

    add http and logs metrics

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,17 +1,20 @@
-import { IMetricsComponent } from "@well-known-components/interfaces"
-import { validateMetricsDeclaration } from "@well-known-components/metrics"
+import { IMetricsComponent } from '@well-known-components/interfaces'
+import { getDefaultHttpMetrics, validateMetricsDeclaration } from '@well-known-components/metrics'
+import { metricDeclarations as logsMetricsDeclarations } from '@well-known-components/logger'
 
 export const metricDeclarations = {
+  ...getDefaultHttpMetrics(),
+  ...logsMetricsDeclarations,
   test_ping_counter: {
-    help: "Count calls to ping",
+    help: 'Count calls to ping',
     type: IMetricsComponent.CounterType,
-    labelNames: ["pathname"]
+    labelNames: ['pathname']
   },
   list_counter: {
-    help: "Count calls to list",
+    help: 'Count calls to list',
     type: IMetricsComponent.CounterType,
-    labelNames: ["pathname"]
-  },
+    labelNames: ['pathname']
+  }
 }
 
 // type assertions
