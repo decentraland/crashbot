38a12aeea68ebd440dd5dbb0fe9be349bdbfb8a3
Hugo Arregui <hugo@decentraland.org>
2023-11-30 15:31:50 -0300

    update deps

--- src/components.ts
+++ src/components.ts
@@ -1,7 +1,7 @@
 import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
 import { createServerComponent, createStatusCheckComponent } from '@well-known-components/http-server'
 import { createLogComponent } from '@well-known-components/logger'
-import { createMetricsComponent } from '@well-known-components/metrics'
+import { createMetricsComponent, instrumentHttpServerWithMetrics } from '@well-known-components/metrics'
 import { AppComponents, GlobalContext } from './types'
 import { metricDeclarations } from './metrics'
 import { createBoltComponent } from './ports/bolt'
@@ -15,7 +15,7 @@ export async function initComponents(): Promise<AppComponents> {
   const logs = await createLogComponent({})
   const server = await createServerComponent<GlobalContext>({ config, logs }, { cors: { maxAge: 36000 } })
   const statusChecks = await createStatusCheckComponent({ server, config })
-  const metrics = await createMetricsComponent(metricDeclarations, { server, config })
+  const metrics = await createMetricsComponent(metricDeclarations, { config })
   const pg = await createPgComponent(
     { logs, config, metrics },
     {
@@ -29,6 +29,8 @@ export async function initComponents(): Promise<AppComponents> {
     }
   )
   const bolt = await createBoltComponent({ pg, config, logs })
+
+  await instrumentHttpServerWithMetrics({ metrics, server, config })
 
   return {
     config,
