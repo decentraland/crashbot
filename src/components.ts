838f659d4a1a39f4cd206f68c2366206bbc1556e
Hugo Arregui <hugo@decentraland.org>
2023-11-30 15:24:01 -0300

    cleanup: use yarn and require slack token and username

--- src/components.ts
+++ src/components.ts
@@ -1,25 +1,20 @@
-import { createDotEnvConfigComponent } from "@well-known-components/env-config-provider"
-import { createServerComponent, createStatusCheckComponent } from "@well-known-components/http-server"
-import { createLogComponent } from "@well-known-components/logger"
-import { createFetchComponent } from "./adapters/fetch"
-import { createMetricsComponent } from "@well-known-components/metrics"
-import { AppComponents, GlobalContext } from "./types"
-import { metricDeclarations } from "./metrics"
-import { createBoltComponent } from "./ports/bolt"
+import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
+import { createServerComponent, createStatusCheckComponent } from '@well-known-components/http-server'
+import { createLogComponent } from '@well-known-components/logger'
+import { createMetricsComponent } from '@well-known-components/metrics'
+import { AppComponents, GlobalContext } from './types'
+import { metricDeclarations } from './metrics'
+import { createBoltComponent } from './ports/bolt'
 import { createPgComponent } from '@well-known-components/pg-component'
 import path from 'path'
 
 // Initialize all the components of the app
 export async function initComponents(): Promise<AppComponents> {
-  const config = await createDotEnvConfigComponent({ path: [".env.default", ".env"] })
+  const config = await createDotEnvConfigComponent({ path: ['.env.default', '.env'] })
 
   const logs = await createLogComponent({})
-  const server = await createServerComponent<GlobalContext>(
-    { config, logs },
-    { cors: { maxAge: 36000 } }
-  )
+  const server = await createServerComponent<GlobalContext>({ config, logs }, { cors: { maxAge: 36000 } })
   const statusChecks = await createStatusCheckComponent({ server, config })
-  const fetch = await createFetchComponent()
   const metrics = await createMetricsComponent(metricDeclarations, { server, config })
   const pg = await createPgComponent(
     { logs, config, metrics },
@@ -29,8 +24,8 @@ export async function initComponents(): Promise<AppComponents> {
         dir: path.resolve(__dirname, 'migrations'),
         migrationsTable: 'pgmigrations',
         ignorePattern: '.*\\.map',
-        direction: 'up',
-      },
+        direction: 'up'
+      }
     }
   )
   const bolt = await createBoltComponent({ pg, config, logs })
@@ -40,7 +35,6 @@ export async function initComponents(): Promise<AppComponents> {
     logs,
     server,
     statusChecks,
-    fetch,
     metrics,
     bolt,
     pg
